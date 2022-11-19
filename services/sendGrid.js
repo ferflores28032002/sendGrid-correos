0215c8ad50c5adf70d33348aae56c479f54b65a9
fernando flores <martinjanns2000@gmail.com>
2022-11-19 13:14:05 -0600

    mosificado

--- services/sendGrid.js
+++ services/sendGrid.js
@@ -1,3 +1,3 @@
 import smgmail from "@sendgrid/mail";
 
-export const sendGrid = smgmail.setApiKey("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");
+export const sendGrid = smgmail.setApiKey(process.env.SENDGRID_API_KEY);
